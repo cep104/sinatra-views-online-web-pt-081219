7efc71ae99cf74f77f0d1380d2053969b4bb896b
Candice Peters <candicelizabethpeters@gmail.com>
2019-11-20 18:59:47 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,14 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
 	get '/' do
+<<<<<<< HEAD
 		erb :index
 	end
 
 	get'/info' do 
 		erb :info
+=======
+		"<h1>Hello World</h1>"
+>>>>>>> f9686ec64df99573513392fa8b5fe25e0eda20f1
 	end
 end
